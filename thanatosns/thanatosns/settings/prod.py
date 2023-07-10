ee1184d93eb4c833a3cc807a860e77a14e7e9fe5
Sinkerine <git@15cm.net>
2023-07-10 00:32:56 -0700

    Support CSRF_TRUSTED_ORIGINS to fix CSRF of https behind reverse proxy

--- thanatosns/thanatosns/settings/prod.py
+++ thanatosns/thanatosns/settings/prod.py
@@ -4,3 +4,7 @@ SECRET_KEY = os.getenv("THANATOSNS_DJANGO_SECRET_KEY", None)
 
 DEBUG = False
 ALLOWED_HOSTS = ["*"]
+
+# Django CSRF doesn't work well behind https reverse proxy.
+if host_url := os.getenv("THANATOSNS_HOST_URL", None):
+    CSRF_TRUSTED_ORIGINS = [host_url]
