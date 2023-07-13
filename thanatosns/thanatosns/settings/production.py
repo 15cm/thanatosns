f548535fad3db8ebb330c9ebe0ec8f098f787bb2
Sinkerine <git@15cm.net>
2023-07-13 01:05:41 -0700

    fix: Capture django log in gunicorn

--- thanatosns/thanatosns/settings/production.py
+++ thanatosns/thanatosns/settings/production.py
@@ -8,3 +8,17 @@ ALLOWED_HOSTS = ["*"]
 # Django CSRF doesn't work well behind https reverse proxy.
 if host_url := os.getenv("THANATOSNS_HOST_URL", None):
     CSRF_TRUSTED_ORIGINS = [host_url]
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "WARNING",
+    },
+}
