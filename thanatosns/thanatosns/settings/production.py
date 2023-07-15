f021a3c62feeeb0e8601c3fc2db8e197891a2849
Sinkerine <git@15cm.net>
2023-07-15 15:08:22 -0700

    doc: Add deployment instructions to readme

--- thanatosns/thanatosns/settings/production.py
+++ thanatosns/thanatosns/settings/production.py
@@ -1,6 +1,6 @@
 from thanatosns.settings.base import *
 
-SECRET_KEY = os.getenv("THANATOSNS_DJANGO_SECRET_KEY", None)
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", None)
 
 DEBUG = False
 ALLOWED_HOSTS = ["*"]
